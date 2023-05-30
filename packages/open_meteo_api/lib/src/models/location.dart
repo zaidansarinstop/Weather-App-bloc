da14476c5e4c45b0b0c5fcc269381ca7d91fbc51
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-05-30 13:28:27 +0530

    creation of weather.g.dart & location.g.dart using build runner

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,8 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final int id;
   final String name;
   final double latitude;
   final double longitude;
 
-  Location(this.id, this.name, this.latitude, this.longitude);
+  Location(
+      {required this.id,
+      required this.name,
+      required this.latitude,
+      required this.longitude});
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
 }
--- packages/open_meteo_api/lib/src/models/weather.dart
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -1,6 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
 class Weather {
   final double temperature;
-  final double weathercode;
+  @JsonKey(name: 'weathercode')
+  final double weatherCode;
 
-  Weather(this.temperature, this.weathercode);
+  Weather({required this.temperature, required this.weatherCode});
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
 }
